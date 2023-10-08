6ff2f4e5b673de6e0b0cbd9c9c082398e4ea8556
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-10-09 02:01:29 +0800

    wip: update server env and package.json

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -3,10 +3,10 @@ import { Environment } from './@types'
 export const Env: Record<string, string> = {
   ENV: process.env.ENV ?? Environment.DEV,
   PORT: process.env.PORT ?? '3000',
-  FE_BASE_URL: process.env.FE_BASE_URL ?? '',
+  FE_BASE_URL: process.env.BASE_URL ?? '',
 }
 
 export const CORS_ORIGIN: Record<Environment, Array<string>> = {
   [Environment.DEV]: ['http://localhost:8080'],
-  [Environment.PROD]: [Env.FE_BASE_URL],
+  [Environment.PROD]: [Env.BASE_URL],
 }
