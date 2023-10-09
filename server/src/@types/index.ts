8b616da16f05a439304b0ccafd572a28842ee6ba
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-10-09 18:25:11 +0800

    wip: add more types

--- server/src/@types/index.ts
+++ server/src/@types/index.ts
@@ -4,3 +4,40 @@ export const Environment = {
 } as const
 
 export type Environment = (typeof Environment)[keyof typeof Environment]
+
+export const Map = {
+  FOREST: 'FOREST',
+} as const
+
+export type Map = (typeof Map)[keyof typeof Map]
+
+export type MapConfig = {
+  key: Map
+  dimensions: {
+    height: number
+    width: number
+  }
+  platform: Array<{
+    height: number
+    width: number
+    x: number
+    y: number
+  }>
+  spawn: {
+    x: number
+    y: number
+  }
+}
+
+export type MapEntities = {
+  WALLS: Array<Matter.Body>
+  PLATFORMS: Array<Matter.Body>
+  PLAYERS: Record<
+    string,
+    {
+      command: Record<string, boolean>
+      body: Matter.Body
+      isInAir: boolean
+    }
+  >
+}
