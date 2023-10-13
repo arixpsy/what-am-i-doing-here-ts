8f7844f839779a8f5c125ef434612501058c9473
arixpsy <arix.phua@ninjavan.co>
2023-10-13 23:04:11 +0800

    feat: create inputController class

---
+++ client/src/utils/inputController.ts
@@ -0,0 +1,54 @@
+import Phaser from 'phaser'
+import { Socket } from 'socket.io-client'
+import { Command } from './../../../server/src/@types'
+
+const commandKeyCodes: Record<Command, number> = {
+	up: Phaser.Input.Keyboard.KeyCodes.W,
+	down: Phaser.Input.Keyboard.KeyCodes.S,
+	left: Phaser.Input.Keyboard.KeyCodes.A,
+	right: Phaser.Input.Keyboard.KeyCodes.D,
+	jump: Phaser.Input.Keyboard.KeyCodes.SPACE,
+} as const
+
+export class InputController {
+	private io?: Socket
+	private input: Record<Command, boolean> = {
+		up: false,
+		down: false,
+		left: false,
+		right: false,
+		jump: false,
+	}
+	private justPressed = false
+
+	constructor(scene: Phaser.Scene, io?: Socket) {
+		this.io = io
+
+		for (const c in this.input) {
+			const command = c as Command
+			const key = scene.input.keyboard?.addKey(commandKeyCodes[command])
+
+			key?.on('down', () => {
+				if (this.input[command] === false) {
+					this.justPressed = true
+				}
+
+				this.input[command] = true
+
+				if (this.justPressed === true) {
+					this.emitInput()
+					this.justPressed = false
+				}
+			})
+
+			key?.on('up', () => {
+				this.input[command] = false
+				this.emitInput()
+			})
+		}
+	}
+
+	emitInput() {
+		this.io?.emit('userCommands', this.input)
+	}
+}
