96695040c99b1e369e6532ee751da54e2dea0ca6
arixpsy <arix.phua@ninjavan.co>
2023-10-14 00:09:16 +0800

    tweak: update env

--- server/src/utils/constants/index.ts
+++ server/src/utils/constants/index.ts
@@ -3,10 +3,10 @@ import { Environment } from './../../@types/index.js'
 export const Env: Record<string, string> = {
   ENV: process.env.ENV ?? Environment.DEV,
   PORT: process.env.PORT ?? '3000',
-  FE_BASE_URL: process.env.BASE_URL ?? '',
+  FE_BASE_URL: process.env.VITE_BASE_URL ?? '',
 }
 
 export const CORS_ORIGIN: Record<Environment, Array<string>> = {
   [Environment.DEV]: ['http://localhost:8080'],
-  [Environment.PROD]: [Env.BASE_URL],
+  [Environment.PROD]: [Env.FE_BASE_URL],
 }
