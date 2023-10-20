79242676606347ee375537ffd6f37fd82c39d057
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2023-10-20 17:51:41 +0800

    feat: complete login scene

--- client/src/scene/Login.ts
+++ client/src/scene/Login.ts
@@ -1,52 +1,63 @@
 import Phaser from 'phaser'
+import { Socket } from 'socket.io-client'
 import { SceneKey } from '../@types/scene'
 import { ImageKey } from '../@types/image'
 import loginForm from './../assets/html/loginForm.html?raw'
+import SpriteData from '../utils/constants/sprite'
+import { Map } from '../../../server/src/@types/map'
 
 export default class Login extends Phaser.Scene {
-  constructor() {
-    super(SceneKey.LOGIN)
-  }
+	private io: Socket | undefined
 
-  create() {
-    this.add.image(500, 250, ImageKey.LOGIN_BACKGROUND)
+	constructor() {
+		super(SceneKey.LOGIN)
+	}
+
+	init() {
+		this.io = this.game.registry.get('socket')
+	}
+
+	create() {
+		this.setupSocket()
+		this.loadBackground()
+		this.loadForm()
+	}
+
+	loadBackground() {
+		this.add.image(500, 250, ImageKey.LOGIN_BACKGROUND)
 		this.add.rectangle(500, 250, 1000, 500, 0x000000, 0.3)
+	}
+
+	loadForm() {
 		const form = this.add.dom(0, 0).createFromHTML(loginForm)
 		form.setOrigin(0, 0)
-		form.addListener('click')
+		form.addListener('submit')
 
-		// const spriteInput = document.getElementById('sprite-input')
-		// for (const sprite of Object.values(SPRITE)) {
-		// 	if (sprite.KEY !== 'PORTAL') {
-		// 		spriteInput.innerHTML += `<option value='${sprite.KEY}'>${sprite.KEY}</option>`
-		// 	}
-		// }
+		const spriteInput = document.getElementById(
+			'sprite-input'
+		) as HTMLSelectElement
+		const displayNameInput = document.getElementById(
+			'display-name-input'
+		) as HTMLInputElement
 
-		// this.element.on('click', (event) => {
-		// 	if (event.target.id == 'loginBtn') {
-		// 		let username = document.getElementById('username-input').value
-		// 		let password = document.getElementById('password-input').value
-		// 		this.scene.start('playGame')
-		// 	}
-		// 	if (event.target.id == 'signUpBtn') {
-		// 		this.scene.start('register')
-		// 	}
-		// 	if (event.target.id == 'instantLoginBtn') {
-		// 		const playerInfo = {
-		// 			uid: uuidv4(),
-		// 			displayName:
-		// 				document.getElementById('display-name-input').value,
-		// 			spriteType: spriteInput.value,
-		// 			channel: 1,
-		// 			map: KEY.SCENE.MAP.FOREST,
-		// 			portal: 0,
-		// 		}
-		// 		if (playerInfo.displayName) {
-		// 			this.scene.start(playerInfo.map, playerInfo)
-		// 		} else {
-		// 			alert('Enter a display name')
-		// 		}
-		// 	}
-		// })
-  }
+		for (const sprite of Object.values(SpriteData)) {
+			spriteInput.innerHTML += `<option value='${sprite.key}'>${sprite.label}</option>`
+		}
+
+		form.on('submit', (event: any) => {
+			event.preventDefault()
+			if (!this.io) return
+
+			this.io.auth = {
+				displayName: displayNameInput.value,
+				spriteType: spriteInput.value,
+			}
+
+			this.io.connect()
+		})
+	}
+
+	setupSocket() {
+		this.io?.on('join map', (mapKey: Map) => this.scene.start(mapKey))
+	}
 }
